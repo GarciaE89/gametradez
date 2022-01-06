10d11c2beead6f4c3b64ffee7890a173216ed78a
Emmmanuel Garcia <Emmanuel_801@hotmail.com>
2022-01-05 19:52:29 -0700

    updated about component

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,10 +4,9 @@ const About = () => {
   return (
     <div>
         <p id="maintext">
-        An online game and game asset trading
-         site.  
-        A fast easy to use, mobile friendly
-         place to post your game stuff for 'sellz'.
+       Gametradez was built on the idea of providing a safe and secure way to buy and sell anything video game related. ~{"\n"}  
+       Gamerz can create user profiles, purchase and post their merchendise. Once a purchase has been initiated Gametradez will act as a broker, ~{"\n"}
+       and confirm the buyer is getting what was posted for sale, once authenticated by Gametradez, the funds are released to the seller.   
         </p>
     </div>
   );
